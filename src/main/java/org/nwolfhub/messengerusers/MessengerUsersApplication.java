b4d15dc5fd24325caf687587910ff242c6052027
s0m31 <admin@nwolfhub.org>
2022-12-01 13:56:12 +0300

    Init project

---
+++ src/main/java/org/nwolfhub/messengerusers/MessengerUsersApplication.java
@@ -0,0 +1,13 @@
+package org.nwolfhub.messengerusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nwolfhub/messengerusers/MessengerUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nwolfhub.messengerusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
