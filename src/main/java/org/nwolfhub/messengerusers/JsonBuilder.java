85c8048b7d94f941f8e5078f0f6982158ed5cbb4
s0m31 <admin@nwolfhub.org>
2022-12-14 12:46:01 +0300

    Finish UsersGet

--- src/main/java/org/nwolfhub/messengerusers/JsonBuilder.java
+++ src/main/java/org/nwolfhub/messengerusers/JsonBuilder.java
@@ -4,7 +4,7 @@ import org.nwolfhub.shared.database.model.User;
 
 public class JsonBuilder {
     public static String successUsersGet(User u) {
-        return "{\"ok\": true, \"user\": {\"is\":" + u.getId() + ", \"username\": \"" + u.getUsername() + "\"}";
+        return "{\"ok\": true, \"user\": {\"id\":" + u.getId() + ", \"username\": \"" + u.getUsername() + "\", \"banned\": \"" + u.banned + "\"}}";
     }
     public static String failUsersGet(String error) {
         return "{\"ok\": false, \"error\": \"" + error + "\"}";
--- src/main/java/org/nwolfhub/messengerusers/api/UserController.java
+++ src/main/java/org/nwolfhub/messengerusers/api/UserController.java
@@ -3,6 +3,9 @@ package org.nwolfhub.messengerusers.api;
 import org.nwolfhub.messengerusers.Auther;
 import org.nwolfhub.messengerusers.JsonBuilder;
 import org.nwolfhub.messengerusers.config.DatabaseConfigurator;
+import org.nwolfhub.shared.database.UserDao;
+import org.nwolfhub.shared.database.model.Dao;
+import org.nwolfhub.shared.database.model.User;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +16,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    private static AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DatabaseConfigurator.class);
 
-    private static Auther auther = new AnnotationConfigApplicationContext(DatabaseConfigurator.class).getBean(Auther.class);
+    private static Auther auther = context.getBean(Auther.class);
+    private static UserDao dao = new UserDao();
+
     @GetMapping("/users/get")
-    public static ResponseEntity<String> get(@RequestParam(name = "username", defaultValue = "", required = false) String username, @RequestParam(name = "id", defaultValue = "", required = false) String id, @RequestHeader(name = "token", defaultValue = "") String token, @RequestHeader(name = "X-Forwarded-For", defaultValue = "0.0.0.0") String ip) {
-        if(ip.equals("0.0.0.0")) return ResponseEntity.status(HttpStatus.FORBIDDEN).body(JsonBuilder.failUsersGet("Direct requests are not allowed"));
-        Integer authId = auther.auth(token);
-        if()
+    public static ResponseEntity<String> get(@RequestParam(name = "username", defaultValue = "", required = false) String username, @RequestParam(name = "id", defaultValue = "", required = false) String id, @RequestHeader(name = "token", defaultValue = "") String token, @RequestHeader(name = "X-Forwarded-For", defaultValue = "0.0.0.0") String ip) throws Auther.RedisNotUsedException {
+        try {
+            if (ip.equals("0.0.0.0"))
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body(JsonBuilder.failUsersGet("Direct requests are not allowed"));
+            try {
+                Integer authId = auther.auth(token);
+                User u = null;
+                if(id.equals("") && !username.equals("")) {
+                    u = (User) dao.get(username);
+                } else if (!id.equals("") && username.equals("")) {
+                    u = dao.getUser(Integer.valueOf(id));
+                }
+                if (u!= null) {
+                    return ResponseEntity.status(HttpStatus.OK).body(JsonBuilder.successUsersGet(u));
+                }
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(JsonBuilder.failUsersGet("User does not exist"));
+            } catch (NullPointerException e) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(JsonBuilder.failUsersGet("Wrong token"));
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(JsonBuilder.failUsersGet("Internal server error: " + e));
+        }
     }
+
 }
